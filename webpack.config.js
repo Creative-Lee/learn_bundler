0a7bb28259d4b45e227b6a445b1b4421520001e3
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-07-02 20:01:42 +0900

    chore: 커스텀 로더 적용 및 경로 수정

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,19 @@ module.exports = {
   entry: {
     index: './src/index.js',
   },
+
   output: {
     filename: '[name].js',
     path: path.resolve('./dist'),
+  },
+
+  // module: 로더 사용을 위한 객체
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // 파일명 or 파일 패턴
+        use: [path.resolve('./loader/dobobLoader.js')], // 로더 함수 경로(여러개 가능)
+      },
+    ],
   },
 };
