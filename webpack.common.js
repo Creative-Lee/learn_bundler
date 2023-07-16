3467feaeb5a7452003b6705f5b10d6d6271cb5e5
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-07-16 12:11:44 +0900

    config: react 기반 환경 설정 준비

--- webpack.common.js
+++ webpack.common.js
@@ -1,37 +1,11 @@
-/** @type {import('webpack').Configuration} */
-
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = {
-  entry: {
-    index: './src/index.js',
-  },
-
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true, // 이전 출력들을 삭제하고 새로 만들어주세요
-  },
-
+  entry: './src/index.tsx',
   module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'], // 로더는 역순으로 실행된다.
-        exclude: /node_modules/,
-      },
-      {
-        test: /(\.jpg|\.png|\.svg)$/,
-        type: 'asset', //  8kb 미만은 asset/inline: data uri 형식으로 번들에 포함, 이상은 asset/resource: 별도의 파일로 내보냄 + 경로 설정
-
-        // generator.filename: 특정 디렉터리에 애셋을 내보낼때 출력 파일명 사용자 정의 - asset, assets/resource 모듈에만 적용가능
-        generator: {
-          filename: 'assets/image/[name][hash][ext]',
-        },
-      },
-    ],
+    rules: [],
   },
-
-  plugins: [new HtmlWebpackPlugin({})],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
 };
