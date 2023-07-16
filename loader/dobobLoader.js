4b1c57d5fd5685f169e809fa6f6f9c572ce9e81e
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-07-16 11:14:51 +0900

    chore: 초기 상태 셋팅

--- loader/dobobLoader.js
+++
@@ -1,5 +0,0 @@
-module.exports = function dobobLoader(content) {
-  console.log('---- 도밥 로더 동작 ----');
-
-  return content.replace('console.log(', 'alert(');
-};
--- plugin/banner.js
+++
@@ -1,3 +0,0 @@
-module.exports = function banner() {
-  return '배너는 자동으로 라이센스가 되네';
-};
